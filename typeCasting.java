06283d0228535841532606c4f7c16edee865ff59
Kspatil1693 <103737429+Kspatil1693@users.noreply.github.com>
2022-04-22 18:05:14 +0530

    Add files via upload
    typecasting file

---
+++ typeCasting.java
@@ -0,0 +1,14 @@
+package Phase1assignment;
+import java.util.*;
+public class typeCasting {
+	public static void main(String[] args) 
+	{
+		char c='T';
+		int a =c;
+		
+		  
+		  System.out.println(+a);
+		
+	}
+
+}
